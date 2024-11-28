5c7c679e8af7f0cfd543e915c4786d1c7bdf8727
“syedmujtaba” <syedmujtabahassan70@gmail.com>
2024-11-28 20:36:03 +0500

    first commit

---
+++ Index.js
@@ -0,0 +1,28 @@
+const shah=require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/home',(req,res)=>{
+
+    res.send("hello from home")
+})
+
+app.get('/shah',(req,res)=>{
+    
+    res.send("<h1>Syed MuJTABA hASSAN SHAH")
+
+        })
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port} `)
+
+
+})
+
